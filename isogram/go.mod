5b9ce433bdcb5e988d998073c349c4c88e246286
Yaroslav Muss <mussyaroslav@gmail.com>
2024-06-07 19:03:06 +0500

    Isogram tests update

---
+++ isogram/go.mod
@@ -0,0 +1,3 @@
+module github.com/mussyaroslav/exercism-go/isogram
+
+go 1.22.3
---
+++ isogram/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestIsIsogram(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{
+			input: "a",
+			want:  true,
+		},
+		{
+			input: "ab",
+			want:  true,
+		},
+		{
+			input: "abb",
+			want:  false,
+		},
+	}
+	for _, test := range tests {
+		if got := IsIsogram(test.input); got != test.want {
+			t.Errorf("IsIsogram(%q) = %t, want %t", test.input, got, test.want)
+		}
+	}
+}
